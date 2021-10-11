f4827e1a8b4964fc90393cd7a09c728b609c2014
ajengayupangesti <56049521+ajengayupangesti@users.noreply.github.com>
2021-10-11 11:15:01 +0700

    view lomba budaya

---
+++ rumahsidoarjo/application/views/lombadanbudaya/detail_lomba.php
@@ -0,0 +1,45 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Detail Lomba Dan Budaya</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <td>Nama Lomba</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Tanggal Publish</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Deskripsi</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Foto</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Link</td>
+                            <td></td>
+                        </tr>
+
+                    </thead>
+                </table>
+                <a href="<?= base_url('LombaDanBudaya') ?>" class="btn btn-danger">Kembali</a>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
